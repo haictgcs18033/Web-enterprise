f3c7f5953a57a9fe96b5deee6e38b0db8a1caefc
Hai <2000caothehai@gmail.com>
2021-02-03 16:30:05 +0700

    fix responsive homepage

--- src/Components/Contribution.js
+++ src/Components/Contribution.js
@@ -4,10 +4,6 @@ import eyeIcon from '../assets/img/eye-icon.png'
 
 export default function Contribution() {
     return (
-        // <div className="col-xl-4 col-lg-6 col-md-6 col-sm-12 d-flex justify-content-center">
-
-        // </div>
-
         <div className="card">
             <img className="card-img-top" src={contribution} alt="123" />
             <div class="overlay"></div>
--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -5,12 +5,12 @@ export default function LoginPage() {
     return (
         <div className="container-login">
             <div className="row my-4">
-                <div className="col-xl-5 px-0">
+                <div className=" col-lg-5 col-xl-5 px-0">
                     <div className="w-100">
                         <img src="/img/cover-1.jpg" alt="123" />
                     </div>
                 </div>
-                <div className="col-xs-12 col-md-12 col-xl-7 px-0">
+                <div className="col-xs-12 col-md-12 col-lg-7 col-xl-7 px-0">
                     <form className="login-form p-4">
                         <h3 className="text-center mb-4" > <span >WELCOME</span> TO SIGN IN</h3>
                         <label>Email</label>
